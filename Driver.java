31eee0f936b18aeb2666a43a99c57b15f94e2dd6
Yolanda Ling <yolandaxiaoqi@gmail.com>
2024-05-27 22:01:53 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
+
+    public static void main(String [] args) throws FileNotFoundException {
+
+       // test constructor w no parameters
+        Polynomial empty_poly = new Polynomial();
+        System.out.println("Evaluate polynomial 0x^0 @ 3: Expected value: 0; Actual value: " + empty_poly.evaluate(3));
+
+        double[] c1 = {7, 3};
+        double[] c2 = {4, 8, 5};
+        //double[] c3 = {19, -13, 4};
+        double[] c4 = {-2};
+
+        int[] e1 = {2, 0};
+        int[] e2 = {3, 2, 1};
+        //int[] e3 = {10, 7, 1};
+        int[] e4 = {7};
+
+        Polynomial p1 = new Polynomial(c1, e1);
+        Polynomial p2 = new Polynomial(c2, e2);
+        Polynomial p3 = new Polynomial(new File("poly.txt")); // test constructor w file parameter
+        Polynomial p4 = new Polynomial(c4, e4);
+
+        // test add method
+        Polynomial sum1 = p1.add(p2);
+        Polynomial sum2 = p3.add(p4);
+        Polynomial sum3 = p2.add(p3);
+
+        System.out.println("Evaluate 4x^3+15x^2+5x+3 @ 2.7: Expected value: 204.582; Actual value: " + sum1.evaluate(2.7));
+        System.out.println("Evaluate 19x^10-15x^7+4x @ 1.3: Expected value: 173.0084; Actual value: " + sum2.evaluate(1.3));
+        System.out.println("Evaluate 19x^10-13x^7+4x^3+8x^2+9x @ -1.1: Expected value: 69.0704; Actual value: " + sum3.evaluate(-1.1));
+
+        // test multiply method
+        Polynomial prod1 = p2.multiply(p1);
+        Polynomial prod2 = p4.multiply(p3);
+        Polynomial prod3 = p3.multiply(p2);
+
+        System.out.println("Evaluate 28x^5+56x^4+47x^3+24x^2+15x @ 1: Expected value: 170; Actual value: " + prod1.evaluate(1));
+        System.out.println("Evaluate -38x^17+26x^14-8x^8 @ -1: Expected value: 56; Actual value: " + prod2.evaluate(-1));
+        System.out.println("Evaluate 76x^13-52x^10+16x^4+152x^12-104x^9+32x^3+95x^11-65x^8+20x^2 @ 1: Expected value: 170; Actual value: " +  prod3.evaluate(1));
+
+        // test hasRoot method
+        if (p1.hasRoot(-2))
+            System.out.println("-2 is a root of 7x^2+3");
         else
-            System.out.println("1 is not a root of s");
+            System.out.println("-2 is not a root of 7x^2+3");
+
+        if (sum2.hasRoot(0))
+            System.out.println("0 is a root of 19x^10-15x^7+4x");
+        else
+            System.out.println("0 is not a root of 19x^10-15x^7+4x");
+
+        // test saveToFile method
+        try {
+			empty_poly.saveToFile("polynomial1.txt");
+			prod3.saveToFile("polynomial2.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.Math;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Polynomial {
 
@@ -16,22 +20,32 @@ public class Polynomial {
         exponents[0] = 0;
     }
 
-    public Polynomial(File file) {
-        String poly = "";
+    public Polynomial(File file) throws FileNotFoundException {
+        String poly;
         Scanner in = new Scanner(file);
+        int index = 0;
 
         poly = in.nextLine();
+        in.close();
 
-        /*
-        go through string + add '+' before every '-'
-        split string using delimiter '+'
-        set length of poly = length of split strings
-        go through each split string
-            split string into using delimiter 'x'
-                first element = coefficient
-                if second element exists, second element = exponent
-        */
+        poly = poly.replace("-", "+-");
+        String[] split = poly.split("\\+");
 
+        coefficients = new double[split.length];
+        exponents = new int[split.length];
+
+        for(int i = 0; i < split.length; i++) {
+            String[] term = split[i].split("x");
+
+            coefficients[index] = Double.parseDouble(term[0]);
+
+            if (term.length > 1)
+                exponents[index] = Integer.parseInt(term[1]);
+            else
+                exponents[index] = 0;
+
+            index += 1;
+        }
     }
 
     public Polynomial(double[] coefficients, int[] exponents) {
@@ -46,66 +60,91 @@ public class Polynomial {
     }
 
     // methods
+    public void saveToFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        String str = "";
+
+        for (int i = 0; i < exponents.length; i++) {
+            str = str.concat(String.valueOf(coefficients[i]));
+            str = str.concat("x");
+            str = str.concat(String.valueOf(exponents[i]));
+
+            if (i != exponents.length - 1 && coefficients[i + 1] > 0)
+                str = str.concat("+");
+        }
+
+        if (file.createNewFile()) {
+            FileWriter output = new FileWriter(file, false);
+            output.write(str);
+            output.close();
+        }
+    }
+
     public Polynomial add(Polynomial p) {
-        double[] sum_coefficients;
-        int[] sum_exponents;
-        ArrayList<Integer> t_exp = new ArrayList<Integer>();
-        int index = -1;
+        double[] sumCoefficients;
+        int[] sumExponents;
+        int length = -1;
+        int index = 0;
 
-        for(int i = 0; i < min(exponents.length, p.exponents.length); i++) {
-            if(!t_exp.contains(exponents[i]))
-                t_exp.add(exponents[i]);
+        length = sumLength(p);
+        sumCoefficients = new double[length];
+        sumExponents = new int[length];
 
-            if(!t_exp.contains(p.exponents[i]))
-                t_exp.add(p.exponents[i]);
-        }
+        for (int i = 0; i < length; i++)
+            sumExponents[i] = -1;
 
-        for(int i = min(exponents.length, p.exponents.length); i < max(exponents.length, p.exponents.length); i++) {
-            if(exponents.length >= i && !t_exp.contains(exponents[i]))
-                t_exp.add(exponents[i]);
-            else if(!t_exp.contains(p.exponents[i]))
-                t_exp.add(p.exponents[i]);
-        }
+        for (int i = 0; i < exponents.length; i++)
+            if (!inArray(sumExponents, exponents[i])) {
+                sumExponents[index] = exponents[i];
+                sumCoefficients[index] = coefficients[i];
+                index += 1;
+            }
+            else
+                sumCoefficients[getIndex(sumExponents, exponents[i])] += coefficients[i];
 
-        sum_exponents = t_exp.sort().toArray();
-        sum_coefficients = new double[sum_exponents.length];
+        for (int i = 0; i < p.exponents.length; i++)
+            if (!inArray(sumExponents, p.exponents[i])) {
+                sumExponents[index] = p.exponents[i];
+                sumCoefficients[index] = p.coefficients[i];
+                index += 1;
+            }
+            else
+                sumCoefficients[getIndex(sumExponents, p.exponents[i])] += p.coefficients[i];
 
-        for(int i = 0; i < exponents.length; i++) {
-            index = t_exp.indexOf(exponents[i]);
-            coefficients[index] += coefficients[i];
-        }
-
-        for(int i = 0; i < p.coefficients.length; i++) {
-            index = t_exp.indexOf(p.exponents[i]);
-            coefficients[index] += p.coefficients[i];
-        }
-
-        return new Polynomial(sum_coefficients, sum_exponents);
+        return new Polynomial(sumCoefficients, sumExponents);
     }
 
     public Polynomial multiply(Polynomial p) {
-        ArrayList<Integer> t_exp = new ArrayList<Integer>();
-        ArrayList<Double> t_coeff = new ArrayList<Double>();
-        int exp = -1;
-        int index = -1;
+        double[] prodCoefficients;
+        int[] prodExponents;
+        int length = -1;
+        int index = 0;
+        int prodExp = -1;
+        double prodCoef = -1;
 
-        for(int i = 0; i < exponents.length; i++) {
-            for(int j = 0; j < p.exponents.length; j++) {
-                exp = exponents[i] * p.exponents[j];
+        length = productLength(p);
+        prodCoefficients = new double[length];
+        prodExponents = new int[length];
 
-                if(t_exp.contains(exp)) {
-                    index = t_exp.indexOf(exp);
-                    t_coeff.set(index, t_coeff.get(index) + coefficients[i] * p.coeffecients[j]);
+        for (int i = 0; i < length; i++)
+            prodExponents[i] = -1;
+
+        for (int i = 0; i < exponents.length; i++) {
+            for (int j = 0; j < p.exponents.length; j++) {
+                prodExp = exponents[i] + p.exponents[j];
+                prodCoef = coefficients[i] * p.coefficients[j];
+
+                if (!inArray(prodExponents, prodExp)) {
+                    prodExponents[index] = prodExp;
+                    prodCoefficients[index] = prodCoef;
+                    index += 1;
                 }
-
-                else {
-                    t_exp.add(exp);
-                    t_coeff.add(coefficients[i] * p.coeffecients[j]);
-                }
+                else
+                    prodCoefficients[getIndex(prodExponents, prodExp)] += prodCoef;
             }
         }
 
-        return new Polynomial(t_coeff.toArray(), t_exp.toArray());
+        return new Polynomial(prodCoefficients, prodExponents);
     }
 
     public double evaluate(double x) {
@@ -119,5 +158,63 @@ public class Polynomial {
 
     public boolean hasRoot(double x) {
         return evaluate(x) == 0;
+    }
+
+    // helper function to determine length of the sum of polynomials
+    private int sumLength(Polynomial p) {
+        int length = exponents.length + p.exponents.length;
+
+        for(int i = 0; i < p.exponents.length; i++) {
+            if (inArray(exponents, p.exponents[i]))
+                length -= 1;
+        }
+
+        return length;
+    }
+
+    // helper function to determine length of the product of polynomials
+    private int productLength(Polynomial p) {
+        int prodExp = -1;
+        int length = -1;
+        int index = 0;
+        int[] exp;
+
+        length = exponents.length * p.exponents.length;
+        exp = new int[length];
+
+        for(int i = 0; i < length; i++)
+            exp[i] = -1;
+
+        for (int i = 0; i < exponents.length; i++)
+            for (int j = 0; j < p.exponents.length; j++) {
+                prodExp = exponents[i] + p.exponents[j];
+
+                if(!inArray(exp, prodExp)) {
+                    exp[index] = prodExp;
+                    index += 1;
+                }
+            }
+
+        return index;
+    }
+
+    // helper function to determine if element is in array
+    private boolean inArray(int[] array, int x) {
+        for(int i = 0; i < array.length; i++) {
+            if(x == array[i])
+                return true;
+        }
+
+        return false;
+    }
+
+    // helper function to get index of element in array
+    private int getIndex(int[] array, int x) {
+        for(int i = 0; i < array.length; i++) {
+            if(x == array[i])
+                return i;
+        }
+
+        return -1;
     }
 }
